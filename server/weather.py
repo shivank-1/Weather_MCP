54b86becf1c2d86008e98a090e2b98c01569a3a6
shivank-1 <shivankdevliyal1@gmail.com>
2025-04-27 23:50:43 +0530

    first commit

--- server/weather.py
+++ server/weather.py
@@ -53,7 +53,7 @@ async def get_alerts(state: str) -> str:
         return "Unable to fetch alerts or no alerts found."
 
     if not data["features"]:
-        return "No active alerts or this state."
+        return "No active alerts for this state."
 
     alerts = [format_alert(feature) for feature in data["features"]]
     return "\n---\n".join(alerts)

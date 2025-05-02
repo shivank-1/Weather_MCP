ce81c66f97da13bcf73fc6804a2f1e821c53ef01
shivank-1 <shivankdevliyal1@gmail.com>
2025-05-02 10:38:50 +0530

    first commit

--- server/weather.py
+++ server/weather.py
@@ -27,9 +27,9 @@ async def make_nws_request(url: str) -> dict[str, Any] | None:
         except Exception:
             return None
         
-# Defining the format of the resons      
+# Defining the format of the resonse            
 def format_alert(feature: dict) -> str:
-    """Format an alert feature into a readable string.""
+    """Format an alert feature into a readable string."""
     props = feature["properties"]
     return f"""
         Event: {props.get('event', 'Unknown')}

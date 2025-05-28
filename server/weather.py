7a474fcd2ce0951688057deb1dd6407116098352
shivank-1 <shivankdevliyal1@gmail.com>
2025-05-28 13:55:48 +0530

    updated

--- server/weather.py
+++ server/weather.py
@@ -7,7 +7,7 @@ from mcp.server.fastmcp import FastMCP
 mcp = FastMCP("weather")
 
 # Constant
-NWS_API_BASE = "https://api.weather.gov"
+NWS_API_BASE = "https://api.weather.gov"]
 USER_AGENT = "weather-app/1.0"
 
 # async function that makes API calls to NWS API - runs in the background without blocking other code

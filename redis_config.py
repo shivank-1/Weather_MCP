41638d52045c2ec23d2114b8aaa20ae90a56794c
shivank-1 <shivankdevliyal1@gmail.com>
2025-06-19 23:02:25 +0530

    Update

--- redis_config.py
+++ redis_config.py
@@ -9,8 +9,7 @@ logger = get_logger()
 # REDIS_HOST = os.getenv("REDIS_HOST", "redis") # Default to "redis" for Docker container
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost") # Default to "localhost" for local development
 REDIS_PORT = os.getenv("REDIS_PORT", "6379"
-REDIS_DB = os.getenv("REDIS_DB", "1")
-
+REDIS_DB = os.getenv("RED
 def get_redis_url():
     logger.debug(f"Using Redis URL: redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}")
     return f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
